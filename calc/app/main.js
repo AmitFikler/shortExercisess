5ba22f576ca0d1a8b11015a94fe24944e7fa09cd
AmitFikler <amitfikler@gmail.com>
2021-10-17 13:29:25 +0300

    add prass numbers option

--- calc/app/main.js
+++ calc/app/main.js
@@ -1,9 +1,29 @@
-import css from "./styles.css";
+// import "./styles.css";
+const plusBtn = document.getElementsByName("plus")[0]
+const subBtn = document.getElementsByName("sub")[0]
+const multiplyBtn = document.getElementsByName("multiply")[0]
+const divideBtn = document.getElementsByName("divide")[0]
+const resultInput = document.querySelector("input") 
+
+plusBtn.addEventListener("click",()=>{
+    console.log("hello");
+})
+
+subBtn.addEventListener("click",()=>{
+    console.log("hello");
+})
+multiplyBtn.addEventListener("click",()=>{
+    console.log("hello");
+})
+divideBtn.addEventListener("click",()=>{
+    console.log("hello");
+})
+
+document.querySelectorAll(".number").forEach(number =>{
+    number.addEventListener("click",pressNumber)
+})
 
 
-function hello() {
-    console.log("hello")
-
+function pressNumber(e){
+    resultInput.value = resultInput.value + e.target.value
 }
-
-hello()
--- calc/webpack.config.js
+++ calc/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const webpack = require('webpack');
+require('webpack');
 
 module.exports = {
   entry: "./app/main.js",
